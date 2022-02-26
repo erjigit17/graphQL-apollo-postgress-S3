631a3a750f2df7948dc48e61ecd67b23cf5919f8
Эржигит Имамов <erjigit17@gmail.com>
2022-02-26 22:32:41 +0600

    bug fix

--- config/config.js
+++ config/config.js
@@ -24,14 +24,11 @@ module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
-    ssl: {
-      rejectUnauthorized: false,
-    },
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-  },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
 };
